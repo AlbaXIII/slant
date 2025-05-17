a9814ce4b89d94e8af18720cd14a3cd6bd2af605
AlbaXIII <alex.jbaker20@googlemail.com>
2025-05-17 15:06:45 +0100

    feat - add navbar & style, create router container for app & index

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/slant-default-image_thumbnail.png";
+import styles from "../styles/NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top" className={styles.navbar}>
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="35" className={styles.brand} />
+          <h2 className={styles.maintitle}>slant.</h2>
+        </Navbar.Brand>
+        <div className={styles.upload}>
+          <i href="#" height="35" class="fa-solid fa-2xl fa-arrow-up-from-bracket"></i>
+          <p>Upload</p>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <Nav.Link>
+              Login
+            </Nav.Link>
+            <Nav.Link>
+              Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
