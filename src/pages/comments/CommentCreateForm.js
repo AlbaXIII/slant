113b0b7ef8e7a3c755f95d8b0a81a33704796059
AlbaXIII <alex.jbaker20@googlemail.com>
2025-05-29 11:06:15 +0100

    maint - attempt overwrite of commentcreate form stage 3 code

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import { axiosRes } from "../../api/axiosDefaults";
+
+function CommentCreateForm(props) {
+  const { article, setArticle, setComments } = props;
+  const [body, setBody] = useState("");
+
+  const handleChange = (event) => {
+    setBody(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        body,
+        article,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setArticle((prevArticle) => ({
+        results: [
+          {
+            ...prevArticle.results[0],
+            comments_count: prevArticle.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setBody("");
+    } catch (err) {
+      //console.log(err);
+    }
+  };
+
+  return (
+    <Form className="mt-2" onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <Form.Control
+            placeholder="thoughts & feelings..."
+            as="textarea"
+            value={body}
+            onChange={handleChange}
+            rows={4}
+            cols={50}
+          />
+        </InputGroup>
+      </Form.Group>
+      <Button
+        disabled={!body.trim()}
+        type="submit"
+        variant="dark"
+      >
+        submit
+      </Button>
+    </Form>
+  );
+}
