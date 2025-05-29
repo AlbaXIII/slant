6bc07f0f41e3c4823b8ce2d685637eed44e7b63c
AlbaXIII <alex.jbaker20@googlemail.com>
2025-05-29 10:36:34 +0100

    maint - debug deployment CommentCreateForm line

--- src/pages/articles/ArticlePage.js
+++ src/pages/articles/ArticlePage.js
@@ -4,7 +4,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
-import CommentCreateForm from "src/pages/comments/CommentCreateForm.js";
+import CommentCreateForm from "../../pages/comments/CommentCreateForm";
 import { useCurrentAuthUser } from "../../contexts/AuthUserContext";
 
 import styles from "../../styles/ArticlePage.module.css";
